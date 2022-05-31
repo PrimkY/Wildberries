fae3d5a5421b42846cf574a2623ea474dd2702d9
PrimkY <fakeprimky@gmail.com>
2022-05-31 20:21:48 +0300

    feat: added styles

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ module.exports = ({ development }) => ({
     main: './src/index.js',
   },
   output: {
-    filename: '[name].[contenthash].js',
+    filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
   },
   module: {
