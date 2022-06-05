0038c8f70c91216f5e541cbb704f3d6121039143
AnastasiaTshnkva <tsuhankova.anastasia@gmail.com>
2022-06-05 21:01:12 +0300

    add search to width less than 992px

--- src/index.js
+++ src/index.js
@@ -5,6 +5,13 @@ const aside = document.querySelector('.menu');
 const close = document.querySelector('.menu__close');
 const burger = document.querySelector('.header__burger');
 const body = document.querySelector('body');
+const header = document.querySelector('.header');
+const headerLogo = document.querySelector('.header__logo');
+const headerLogoSmall = document.querySelector('.header__logo-small');
+const searchBox = document.querySelector('.header__search-box');
+const searchClose = document.querySelector('.header__search-close');
+const searchMagnifier = document.querySelector('.fa-magnifying-glass');
+console.log(headerLogoSmall);
 
 burger.addEventListener('click', () => {
   aside.style.display = 'block';
@@ -13,5 +20,20 @@ burger.addEventListener('click', () => {
 close.addEventListener('click', () => {
   aside.style.display = 'none';
   body.style.overflowY = 'auto';
+});
+
+searchMagnifier.addEventListener('click', () => {
+  searchBox.classList.add('header__search-box-visible');
+  searchClose.classList.add('visible');
+  headerLogo.classList.add('hidden');
+  headerLogoSmall.classList.add('hidden');
+  header.classList.add('header-search-visible');
+});
+searchClose.addEventListener('click', () => {
+  searchBox.classList.remove('header__search-box-visible');
+  searchClose.classList.remove('visible');
+  headerLogo.classList.remove('hidden');
+  headerLogoSmall.classList.remove('hidden');
+  header.classList.remove('header-search-visible');
 });
 slider();
