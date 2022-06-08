c74834c974f8cb0b6e77fc69786ca4c284ce1523
AbzMijon <egorchiktop228@gmail.com>
2022-06-09 00:21:11 +0300

    fix height item

--- src/index.js
+++ src/index.js
@@ -30,112 +30,106 @@ const addStorageItems = async() => {
   const response = await fetch('http://ec2-3-91-9-40.compute-1.amazonaws.com:31337/products/');
   const cards = await response.json();
 
-  console.log(localStorage);
   if(typeof localStorage === 'null') {
     localStorage.setItem('items', JSON.stringify(cards));
-  } else {
   }
 
 
   const addItem = (category, title, discount, price, count, url, id) => {
-  const liItem = document.createElement('li');
-  const card = document.createElement('div');
-  const img = document.createElement('img');
-  const fastCheck = document.createElement('a');
-  const discountPop = document.createElement('span');
-  const addBtn = document.createElement('button');
-  const priceRow = document.createElement('div');
-  const priceNow = document.createElement('p');
-  const addBtnPlus = document.createElement('i');
-  const nameItem = document.createElement('div');
-  const hiddenBlock = document.createElement('div');
-  const addMinus = document.createElement('i');
-  const counter = document.createElement('span');
-  const addPlus = document.createElement('i');
+    const liItem = document.createElement('li');
+    const card = document.createElement('div');
+    const img = document.createElement('img');
+    const fastCheck = document.createElement('a');
+    const discountPop = document.createElement('span');
+    const addBtn = document.createElement('button');
+    const priceRow = document.createElement('div');
+    const priceNow = document.createElement('p');
+    const addBtnPlus = document.createElement('i');
+    const nameItem = document.createElement('div');
+    const hiddenBlock = document.createElement('div');
+    const addMinus = document.createElement('i');
+    const counter = document.createElement('span');
+    const addPlus = document.createElement('i');
 
-  if(count > 0) {
-    addBtn.style.display = 'block';
-  }
-
-  card.dataset.id = id;
-  const getImage = JSON.parse(localStorage.getItem('items'));
-  img.src = getImage[id-1].image_url;
-
-  const fastCheckTxtNode = document.createTextNode('Быстрый просмотр');
-  const discountPopTxtNode = document.createTextNode(discount + '%');
-  const priceNowTxt = document.createTextNode(price + ' BYN');
-  const nameItemTxtNode = document.createTextNode(title +'');
-
-  fastCheck.append(fastCheckTxtNode);
-  discountPop.append(discountPopTxtNode);
-  priceNow.append(priceNowTxt);
-  nameItem.append(nameItemTxtNode);
-
-  liItem.className = 'popular__item';
-  card.className = 'popular__card';
-  img.className = 'popular__img';
-  fastCheck.className = 'popular__fast-check';
-  discountPop.className = 'popular__discount';
-  addBtn.className = 'popular__add';
-  addBtnPlus.className = 'fa-solid fa-plus';
-  priceRow.className = 'popular__price-row';
-  priceNow.className = 'popular__price-now';
-  nameItem.className = 'popular__name';
-  addMinus.className = 'fa-solid fa-minus minus--hidden'
-  addPlus.className = 'fa-solid fa-plus plus--hidden';
-  hiddenBlock.className = 'popular__hidden-block';
-
-  liItem.append(card);
-  card.append(img);
-  card.append(fastCheck);
-  card.append(discountPop);
-  addBtn.append(addBtnPlus);
-  card.append(addBtn);
-  liItem.append(priceRow);
-  priceRow.append(priceNow);
-  row.append(liItem);
-  liItem.append(nameItem);
-
-  if(count > 0) {
-    card.append(hiddenBlock);
-    hiddenBlock.style.display = 'block';
-  } else {
-    card.append(hiddenBlock);
-  }
-
-  hiddenBlock.append(addMinus);
-  hiddenBlock.append(counter);
-  hiddenBlock.append(addPlus);
-
-  let num = count;
-  counter.innerText = num;
-
-  addBtn.addEventListener('click', () => {
-    addBtn.style.display = 'none';
-    hiddenBlock.style.display = 'flex';
-  })
-
-  hiddenBlock.addEventListener('click', (event) => {
-    if (event.target === addPlus) {
+    if(count > 0) {
+      addBtn.style.display = 'block';
     }
-  });
+
+    card.dataset.id = id;
+    const getImage = JSON.parse(localStorage.getItem('items'));
+    img.src = getImage[id-1].image_url;
+
+    const fastCheckTxtNode = document.createTextNode('Быстрый просмотр');
+    const discountPopTxtNode = document.createTextNode(discount + '%');
+    const priceNowTxt = document.createTextNode(price + ' BYN');
+    const nameItemTxtNode = document.createTextNode(title +'');
+
+    fastCheck.append(fastCheckTxtNode);
+    discountPop.append(discountPopTxtNode);
+    priceNow.append(priceNowTxt);
+    nameItem.append(nameItemTxtNode);
+
+    liItem.className = 'popular__item';
+    card.className = 'popular__card';
+    img.className = 'popular__img';
+    fastCheck.className = 'popular__fast-check';
+    discountPop.className = 'popular__discount';
+    addBtn.className = 'popular__add';
+    addBtnPlus.className = 'fa-solid fa-plus';
+    priceRow.className = 'popular__price-row';
+    priceNow.className = 'popular__price-now';
+    nameItem.className = 'popular__name';
+    addMinus.className = 'fa-solid fa-minus minus--hidden'
+    addPlus.className = 'fa-solid fa-plus plus--hidden';
+    hiddenBlock.className = 'popular__hidden-block';
+
+    liItem.append(card);
+    card.append(img);
+    card.append(fastCheck);
+    card.append(discountPop);
+    addBtn.append(addBtnPlus);
+    card.append(addBtn);
+    liItem.append(priceRow);
+    priceRow.append(priceNow);
+    row.append(liItem);
+    liItem.append(nameItem);
+
+    if(count > 0) {
+      card.append(hiddenBlock);
+      hiddenBlock.style.display = 'block';
+      counter.innerText = count;
+      hiddenBlock.style.display = 'flex';
+    } else {
+      card.append(hiddenBlock);
+    }
+
+    hiddenBlock.append(addMinus);
+    hiddenBlock.append(counter);
+    hiddenBlock.append(addPlus);
+
+
+    addBtn.addEventListener('click', () => {
+      addBtn.style.display = 'none';
+      hiddenBlock.style.display = 'flex';
+    });
 
     addBtn.addEventListener('click', (event) => {
       const getCards = JSON.parse(localStorage.getItem('items'));
       const currentElem = event.target.closest('.popular__card');
       const selectedTodo = getCards.find(
-      (item) => +item.id === +currentElem.dataset.id
+        (item) => +item.id === +currentElem.dataset.id
       );
       selectedTodo.count++;
+      counter.innerText = selectedTodo.count;
       localStorage.setItem('items', JSON.stringify(getCards));
     })
-    
+
 
     addPlus.addEventListener('click', (event) => {
       const getCards = JSON.parse(localStorage.getItem('items'));
       const currentElem = event.target.closest('.popular__card');
       const selectedTodo = getCards.find(
-      (item) => +item.id === +currentElem.dataset.id
+        (item) => +item.id === +currentElem.dataset.id
       );
       selectedTodo.count++;
       counter.innerText = selectedTodo.count;
@@ -147,18 +141,19 @@ const addStorageItems = async() => {
       const currentElem = event.target.closest('.popular__card');
       const selectedTodo = getCards.find(
         (item) => +item.id === +currentElem.dataset.id
-        );
-        if(selectedTodo.count < 1) {
-          
-        } else {
-          selectedTodo.count--;
-          counter.innerText = selectedTodo.count;
-          localStorage.setItem('items', JSON.stringify(getCards));
-        }
-        if(selectedTodo.count === 0) {
-          const newBlock = currentElem.querySelector('.popular__hidden-block');
-          hiddenBlock.style.display = 'hidden';
-        }
+      );
+      if(selectedTodo.count < 1) {
+
+      } else {
+        selectedTodo.count--;
+        counter.innerText = selectedTodo.count;
+        localStorage.setItem('items', JSON.stringify(getCards));
+      }
+      if(selectedTodo.count === 0) {
+        const newBlock = currentElem.querySelector('.popular__hidden-block');
+        hiddenBlock.style.display = 'none';
+        addBtn.style.display = 'block';
+      }
     });
   };
   const getCard = () => {
@@ -220,7 +215,7 @@ const addStorageItems = async() => {
         addItem(randCard.category, randCard.title, randCard.discount, randCard.price, randCard.count, randCard.url, randCard.id);
       };
     }
-};
+  };
   return getCard();
 };
 
