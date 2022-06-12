161ae699cf68a37e4c2f4316b41586ad971748d4
AbzMijon <egorchiktop228@gmail.com>
2022-06-12 20:16:48 +0300

    add logic of basket

--- src/assets/scripts/slider.js
+++ src/assets/scripts/slider.js
@@ -1,3 +1,10 @@
+import { ChiefSlider } from './chief-slider'
 export function slider() {
-
+  document.addEventListener('DOMContentLoaded', function () {
+    const sliders = new ChiefSlider('.slider', {
+      loop: true,
+      autoplay: true,
+      interval: 5000,
+    });
+  });
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
+// @ts-ignore
+import './assets/styles/chief-slider.min.css'
 import './style.scss';
+import { slider } from "./assets/scripts/slider";
+
+slider();
 
 const aside = document.querySelector('.menu');
 const close = document.querySelector('.menu__close');
@@ -40,6 +45,7 @@ const addStorageItems = async () => {
   );
   const cards = await response.json();
 
+
   if (localStorage.length === 0) {
     localStorage.setItem('items', JSON.stringify(cards));
   }
@@ -53,6 +59,7 @@ const addStorageItems = async () => {
     const addBtn = document.createElement('button');
     const priceRow = document.createElement('div');
     const priceNow = document.createElement('p');
+    const priceLater = document.createElement('p');
     const addBtnPlus = document.createElement('i');
     const nameItem = document.createElement('div');
     const hiddenBlock = document.createElement('div');
@@ -68,14 +75,17 @@ const addStorageItems = async () => {
     const getImage = JSON.parse(localStorage.getItem('items'));
     img.src = getImage[id - 1].image_url;
 
+
     const fastCheckTxtNode = document.createTextNode('Быстрый просмотр');
     const discountPopTxtNode = document.createTextNode(discount + '%');
     const priceNowTxt = document.createTextNode(price + ' BYN');
     const nameItemTxtNode = document.createTextNode(title + '');
+    const priceLaterTxt = document.createTextNode(Math.ceil(price / (100 - discount) * 100)  + ' BYN');
 
     fastCheck.append(fastCheckTxtNode);
     discountPop.append(discountPopTxtNode);
     priceNow.append(priceNowTxt);
+    priceLater.append(priceLaterTxt);
     nameItem.append(nameItemTxtNode);
 
     liItem.className = 'popular__item';
@@ -87,6 +97,7 @@ const addStorageItems = async () => {
     addBtnPlus.className = 'fa-solid fa-plus';
     priceRow.className = 'popular__price-row';
     priceNow.className = 'popular__price-now';
+    priceLater.className = 'popular__price-later';
     nameItem.className = 'popular__name';
     addMinus.className = 'fa-solid fa-minus minus--hidden';
     addPlus.className = 'fa-solid fa-plus plus--hidden';
@@ -100,6 +111,7 @@ const addStorageItems = async () => {
     card.append(addBtn);
     liItem.append(priceRow);
     priceRow.append(priceNow);
+    priceRow.append(priceLater);
     row.append(liItem);
     liItem.append(nameItem);
 
@@ -161,7 +173,94 @@ const addStorageItems = async () => {
         addBtn.style.display = 'block';
       }
     });
+
+    fastCheck.addEventListener('click', () => {
+      const fastCheckBlock = document.createElement('div');
+      fastCheckBlock.dataset.id = id;
+      const fastCheckClose = document.createElement('div');
+      const fastCheckBlockImg = document.createElement('div');
+      const fastCheckBlockDescription = document.createElement('div');
+      const fastCheckBlockDescriptionTitle = document.createElement('h2');
+      const fastCheckBlockDescriptionTitleText = document.createTextNode(title);
+      const fastCheckBlockDescriptionPrice = document.createElement('h3');
+      const fastCheckBlockDescriptionPriceText = document.createTextNode('Стоимость: '+ price + ' BYN' )
+      const fastCheckBlockDescriptionCategory = document.createElement('h3');
+      const fastCheckBlockDescriptionCategoryText = document.createTextNode('Категория: ' + category);
+      const fastCheckBlockDescriptionDiscount = document.createElement('h3');
+      const fastCheckBlockDescriptionDiscountText = document.createTextNode('Вы экономите: ' + discount + '%');
+      const fastCheckBlockDescriptionBtn = document.createElement('button');
+      let fastCheckBlockDescriptionBtnText = document.createTextNode('Добавить в корзину');
+
+
+      if (counter.innerText > 0){
+        fastCheckBlockDescriptionBtnText = ('В корзине ' + counter.innerText + ' шт.');
+      };
+
+      fastCheckBlock.className = 'fastCheckBlock';
+      fastCheckBlockImg.className = 'fastCheckBlockImg';
+      fastCheckBlockDescription.className = 'fastCheckBlockDescription';
+      fastCheckClose.className = 'fastCheckClose fa-regular fa-circle-xmark fa-2x';
+      fastCheckBlockDescriptionCategory.className = 'fastCheckBlockDescriptionCategory';
+      fastCheckBlockDescriptionBtn.className = 'fastCheckBlockDescriptionBtn title3';
+
+      fastCheckBlock.append(fastCheckClose);
+      fastCheckBlockImg.append(img);
+      fastCheckBlock.append(fastCheckBlockImg);
+      fastCheckBlockDescriptionTitle.append(fastCheckBlockDescriptionTitleText);
+      fastCheckBlockDescription.append(fastCheckBlockDescriptionTitle);
+      fastCheckBlockDescriptionPrice.append(fastCheckBlockDescriptionPriceText);
+      fastCheckBlockDescription.append(fastCheckBlockDescriptionPrice)
+      fastCheckBlockDescriptionCategory.append(fastCheckBlockDescriptionCategoryText);
+      fastCheckBlockDescription.append(fastCheckBlockDescriptionCategory);
+      fastCheckBlockDescriptionDiscount.append(fastCheckBlockDescriptionDiscountText);
+      fastCheckBlockDescription.append(fastCheckBlockDescriptionDiscount);
+      fastCheckBlockDescriptionBtn.append(fastCheckBlockDescriptionBtnText);
+      fastCheckBlockDescription.append(fastCheckBlockDescriptionBtn)
+      fastCheckBlock.append(fastCheckBlockDescription);
+      body.append(fastCheckBlock);
+
+      fastCheckBlockDescriptionBtn.addEventListener('click', (event) =>{
+        const getCards = JSON.parse(localStorage.getItem('items'));
+        const currentElem = event.target.closest('.fastCheckBlock');
+        console.log(currentElem);
+        const selectedTodo = getCards.find(
+          (item) => +item.id === +currentElem.dataset.id
+        );
+        selectedTodo.count++;
+        console.log(selectedTodo);
+        fastCheckBlockDescriptionBtn.innerText = ('В корзине ' + selectedTodo.count + ' шт.');
+        localStorage.setItem('items', JSON.stringify(getCards));
+      });
+      fastCheckClose.addEventListener('click', () => {
+        const getCards = JSON.parse(localStorage.getItem('items'));
+        const currentElem = event.target.closest('.fastCheckBlock');
+        console.log(currentElem);
+        const selectedTodo = getCards.find(
+          (item) => +item.id === +currentElem.dataset.id
+        );
+        if (selectedTodo.count > 0) {
+          fastCheckBlock.remove();
+          card.append(img);
+          addBtn.append(addBtnPlus);
+          card.append(addBtn);
+          card.append(hiddenBlock);
+          card.append(discountPop);
+          addBtn.style.display = 'none';
+          hiddenBlock.style.display = 'flex';
+          counter.innerText = selectedTodo.count;
+        } else {
+          fastCheckBlock.remove();
+          card.append(img);
+          addBtn.append(addBtnPlus);
+          card.append(addBtn);
+          card.append(hiddenBlock);
+          card.append(discountPop);
+        }
+      });
+
+    });
   };
+
   const getCard = () => {
     function randomItems() {
       for (let i = 0; i < 6; i++) {
@@ -267,6 +366,27 @@ const addStorageItems = async () => {
           randCard.id
         );
       }
+    } else if(lastElem === 'basket.html') {
+      row.style.gridTemplateColumns = '18%';
+      const randCardId = allItems.filter(elem => {
+        return elem.count > 0;
+      });
+      let totalPrice = 0;
+      for (let i = 0; i < randCardId.length; i++) {
+        let randCard = randCardId[i];
+        addItem(
+          randCard.category,
+          randCard.title,
+          randCard.discount,
+          randCard.price,
+          randCard.count,
+          randCard.url,
+          randCard.id
+        );
+        totalPrice += randCardId[i].price;
+      }
+      const totalHtmlPrice = document.querySelector('.popular__total-price');
+      totalHtmlPrice.innerText = Math.round(totalPrice) + ' BYN';
     } else {
       randomItems();
     }
@@ -301,10 +421,31 @@ const addStorageItems = async () => {
           row.style.gridTemplateColumns = 'repeat(auto-fit, minmax(175px, 1fr)';
         }
       }
+      row.classList.add('grid');
       if (popularRow.innerHTML) {
         popularTitle.innerHTML = 'Результат поиска:';
       } else {
         popularTitle.innerHTML = 'Ничего не найдено';
+      }
+    }
+  });
+    // enter
+  searchInput.addEventListener('keydown', (event) => {
+    if (searchInput.value) {
+      if(event.keyCode === 13) {
+        const itemsArr = JSON.parse(localStorage.getItem('items'));
+        popularRow.innerHTML = '';
+        for (let i = 0; i < itemsArr.length; i++) {
+          if (itemsArr[i].title.toLowerCase().includes(searchInput.value.toLowerCase())) {
+            addItem(itemsArr[i].category, itemsArr[i].title, itemsArr[i].discount, itemsArr[i].price, itemsArr[i].count, itemsArr[i].url, itemsArr[i].id);
+          }
+        }
+        row.classList.add('grid');
+        if (popularRow.innerHTML) {
+          popularTitle.innerHTML = 'Результат поиска:';
+        } else {
+          popularTitle.innerHTML = 'Ничего не найдено';
+        }
       }
     }
   });
@@ -320,16 +461,16 @@ const headerLogo = document.querySelector('.header__logo');
 const headerLogoSmall = document.querySelector('.header__logo-small');
 const searchBox = document.querySelector('.header__search-box');
 const searchClose = document.querySelector('.header__search-close');
-const searchMagnifier = document.querySelector('.fa-magnifying-glass');
+const searchMagnifyingGlass = document.querySelector('.fa-magnifying-glass');
 
-searchMagnifier.addEventListener('click', () => {
+  searchMagnifyingGlass.addEventListener('click', () => {
   searchBox.classList.add('header__search-box-visible');
   searchClose.classList.add('visible');
   headerLogo.classList.add('hidden');
   headerLogoSmall.classList.add('hidden');
   header.classList.add('header-search-visible');
 });
-searchClose.addEventListener('click', () => {
+  searchClose.addEventListener('click', () => {
   searchBox.classList.remove('header__search-box-visible');
   searchClose.classList.remove('visible');
   headerLogo.classList.remove('hidden');
